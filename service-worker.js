f7c27f4e165b7d10965b4b0816db8b30882f78c5
Candy match <askirar275@gmail.com>
2025-10-06 19:20:32 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,18 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("candy-v2").then(cache => cache.addAll([
+      "./",
+      "index.html",
+      "css/home.css",
+      "css/game.css",
+      "js/home.js",
+      "js/game.js"
+    ]))
+  );
+});
 
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
