51b7b1b8e3ab94dd71ee0ea4674233a2284dc136
Candy match <askirar275@gmail.com>
2025-10-13 13:41:47 +0530

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,31 +1,9 @@
-// storage.js
-console.log('Loaded: storage.js');
 window.StorageAPI = {
-  getCoins(){
-    return Number(localStorage.getItem('cm_coins') || 0);
-  },
-  addCoins(n){
-    let c = this.getCoins() + Number(n||0);
-    if(c<0) c=0;
-    localStorage.setItem('cm_coins', c);
-    return c;
-  },
-  getLevel(){
-    return Number(localStorage.getItem('cm_level') || 1);
-  },
-  setLevel(l){
-    localStorage.setItem('cm_level', Number(l||1));
-  },
-  reset(){
-    localStorage.removeItem('cm_coins');
-    localStorage.removeItem('cm_level');
-    localStorage.removeItem('cm_playLevel');
-    console.log('Storage reset');
-  },
-  setPlayLevel(l){
-    localStorage.setItem('cm_playLevel', Number(l||1));
-  },
-  getPlayLevel(){
-    return Number(localStorage.getItem('cm_playLevel') || this.getLevel() || 1);
-  }
+  getCoins(){ return Number(localStorage.getItem('candy_coins') || 0); },
+  addCoins(n){ const c = this.getCoins() + Number(n||0); localStorage.setItem('candy_coins', c); return c; },
+  getLevel(){ return Number(localStorage.getItem('candy_level') || 1); },
+  setLevel(l){ localStorage.setItem('candy_level', Number(l)); },
+  getPlayLevel(){ return Number(localStorage.getItem('candy_play') || this.getLevel() || 1); },
+  setPlayLevel(l){ localStorage.setItem('candy_play', Number(l)); },
+  reset(){ localStorage.clear(); alert('Progress reset!'); }
 };
