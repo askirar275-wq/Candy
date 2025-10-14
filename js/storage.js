09a1ff92382e4ef8538178980d507145d8dcfbba
Candy match <askirar275@gmail.com>
2025-10-14 20:19:33 +0530

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,11 +1,21 @@
-// small StorageAPI wrapper
-window.StorageAPI = (function(){
-  const KEY_COINS = 'candy_coins';
-  const KEY_LEVEL = 'candy_level';
-  function getCoins(){ return Number(localStorage.getItem(KEY_COINS) || 0); }
-  function addCoins(n){ localStorage.setItem(KEY_COINS, getCoins()+Number(n||0)); }
-  function setLevel(l){ localStorage.setItem(KEY_LEVEL, Number(l||1)); }
-  function getLevel(){ return Number(localStorage.getItem(KEY_LEVEL) || 1); }
-  return { getCoins, addCoins, setLevel, getLevel };
-})();
-console.log('Loaded: js/storage.js');
+// Local Storage system
+const StorageAPI = {
+  data: { coins: 0, level: 1 },
+
+  load() {
+    const d = localStorage.getItem("CandyData");
+    if (d) this.data = JSON.parse(d);
+  },
+
+  save() {
+    localStorage.setItem("CandyData", JSON.stringify(this.data));
+  },
+
+  getCoins() { return this.data.coins; },
+  addCoins(n) { this.data.coins += n; this.save(); },
+
+  getLevel() { return this.data.level; },
+  setLevel(n) { this.data.level = n; this.save(); }
+};
+
+StorageAPI.load();
