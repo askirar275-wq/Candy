70f96edf9d3397b569e169ee149e5e755e2f7840
Candy match <askirar275@gmail.com>
2025-10-16 10:17:29 +0530

    Create level-data.js

---
+++ js/level-data.js
@@ -0,0 +1,10 @@
+// js/level-data.js
+window.LevelData = [
+  { id:1, goal:500, moves:30, reward:50, boardSize:7 },
+  { id:2, goal:1000, moves:28, reward:80, boardSize:7 },
+  { id:3, goal:1500, moves:26, reward:120, boardSize:8 },
+  { id:4, goal:2000, moves:24, reward:160, boardSize:8 },
+  { id:5, goal:2500, moves:22, reward:200, boardSize:9 }
+  // आगे add करो जैसे चाहो
+];
+console.log('Loaded: js/level-data.js');
