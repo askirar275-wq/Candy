5b6ebdc38a5176af14458a46f8325234a348aa5c
Candy match <askirar275@gmail.com>
2025-10-16 11:03:16 +0530

    Update level data with new goals and moves

--- js/level-data.js
+++ js/level-data.js
@@ -1,10 +1,9 @@
-// js/level-data.js
 window.LevelData = [
-  { id:1, goal:500, moves:30, reward:50, boardSize:7 },
-  { id:2, goal:1000, moves:28, reward:80, boardSize:7 },
-  { id:3, goal:1500, moves:26, reward:120, boardSize:8 },
-  { id:4, goal:2000, moves:24, reward:160, boardSize:8 },
-  { id:5, goal:2500, moves:22, reward:200, boardSize:9 }
-  // आगे add करो जैसे चाहो
+  { id: 1, goal: 500, moves: 25, reward: 50 },
+  { id: 2, goal: 1000, moves: 25, reward: 60 },
+  { id: 3, goal: 1500, moves: 30, reward: 80 },
+  { id: 4, goal: 2000, moves: 35, reward: 100 },
+  { id: 5, goal: 2500, moves: 35, reward: 120 },
+  { id: 6, goal: 3000, moves: 40, reward: 150 },
 ];
-console.log('Loaded: js/level-data.js');
+console.log("Loaded: level-data.js");
