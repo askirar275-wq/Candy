0aa79deaf010b27bc0ad374d83155732286d82f1
Candy match <askirar275@gmail.com>
2025-10-20 17:41:07 +0530

    Delete js/level-data.js

--- js/level-data.js
+++
@@ -1,9 +0,0 @@
-window.LevelData = [
-  { id: 1, goal: 500, moves: 25, reward: 50 },
-  { id: 2, goal: 1000, moves: 25, reward: 60 },
-  { id: 3, goal: 1500, moves: 30, reward: 80 },
-  { id: 4, goal: 2000, moves: 35, reward: 100 },
-  { id: 5, goal: 2500, moves: 35, reward: 120 },
-  { id: 6, goal: 3000, moves: 40, reward: 150 },
-];
-console.log("Loaded: level-data.js");
