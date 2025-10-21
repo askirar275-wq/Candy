95ca2d5529044086ae66b10d83e039bd14c053ad
Candy match <askirar275@gmail.com>
2025-10-21 16:11:11 +0530

    Update sound.js

--- js/sound.js
+++ js/sound.js
@@ -1,17 +1,27 @@
 // js/sound.js
 (function(){
-  const sounds = {
-    pop: new Audio('sounds/pop.mp3'),
-    win: new Audio('sounds/win.mp3')
-  };
-  sounds.pop.volume = 0.3;
-  sounds.win.volume = 0.4;
+  const sounds = {};
+  function load(name, path){
+    try{
+      const a = new Audio(path);
+      a.preload = 'auto';
+      sounds[name] = a;
+    }catch(e){
+      console.warn('sound load err', name, path, e);
+    }
+  }
+
+  load('pop', 'sounds/pop.mp3');
+  load('win', 'sounds/win.mp3');
 
   window.SFX = {
     play(name){
       const s = sounds[name];
       if(!s) return;
-      try{ s.currentTime = 0; s.play(); }catch(e){}
+      try{
+        s.currentTime = 0;
+        s.play();
+      }catch(e){ /* user gesture required? ignore */ }
     }
   };
 })();
